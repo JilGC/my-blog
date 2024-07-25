de69d87385a95cf5a23c9ef103de978cac6a730a
JilGC <105502236+JilGC@users.noreply.github.com>
2024-07-25 09:24:51 -0300

    previous updates

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import NotFoundPage from './NotFoundPage';
 import articles from "./article-content";
+
+
+
 const ArticlePage = () => {
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] })
+
+    useEffect(() => {
+        setArticleInfo({ upvotes: 3, comments: [] });
+    });
+
     const { articleId } = useParams();
     const article = articles.find((article) => article.name === articleId);
 
@@ -12,6 +23,7 @@ const ArticlePage = () => {
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
             {
                 article.content.map((paragraph, i) => (
                     <p key={i}>{paragraph}</p>))
